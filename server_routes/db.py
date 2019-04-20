741ead2a937e7b6022a3de6ef558ce8757e25ac4
Denis Soldatov <moralarch@gmail.com>
2019-04-20 15:31:11 +0700

    rc history

--- server_routes/db.py
+++ server_routes/db.py
@@ -4,7 +4,7 @@ from werkzeug.contrib.cache import SimpleCache
 from server_routes import recogniser
 from server_routes import server
 from base64 import b64encode
-
+from datetime import datetime
 
 def sql_execute(sql_give):
     conn = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost')
@@ -46,6 +46,33 @@ def add_user(user):
     print(sql)
 
 
+def sql_execute_history(sql):
+    conn = psycopg2.connect(dbname='history', user='postgres', password='postgres', host='localhost')
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    answer = None
+    try:
+        answer = cursor.fetchall()
+    except:
+        pass
+    finally:
+        conn.close()
+        cursor.close()
+        return answer
+
+
+def add_history(id,status):
+    sql = '''
+        select status
+        from table t
+        where id = '{id}'
+        order by dt
+        limit 1
+    '''.format(id = id)
+    if status != sql_execute_history(sql):
+        sql = """INSERT INTO history(id,status,date)
+                 VALUES ('{id}','{status}','{date}')""".format(id = id,status = status,date = str(datetime.now()))
+        sql_execute_history(sql)
 def update(id, status):
+    add_history(id,status)
     sql = "UPDATE users2 SET status  = '{status}' WHERE id = {id} ;".format(status=status, id=id)
     sql_execute(sql)
